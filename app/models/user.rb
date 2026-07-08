5f97865f13e1b8cf79161dc5d476615d4fb7ac96
gothic459 <d.sek464@gmail.com>
2026-07-08 15:15:42 +0200

    add form component, fix up sign up modal, fix up google oauth2, logging in with google is now possible

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,23 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
 
+  def initials
+    [first_name, last_name].map { |n| n&.first&.upcase }.compact.join
+  end
+
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
 
     unless user
-        user = User.create(name: data['name'],
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
-        )
+      user = User.create(
+        first_name: data['first_name'],
+        last_name: data['last_name'],
+        email: data['email'],
+        password: Devise.friendly_token[0, 20],
+        provider: access_token.provider,
+        uid: access_token.uid
+      )
     end
     user
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,2 @@
-OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:5100'
+#todo: change host after domain done
+OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : "http://localhost:#{ENV.fetch('PORT', 3000)}"
